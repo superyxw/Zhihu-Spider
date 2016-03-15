af3777deac19918e8f455400323040ac609fd9a2
starkwang <381152119@qq.com>
2016-03-15 10:00:45 +0800

    add getUserHashID

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var Spider = require('./Spider');
-Spider().request({
-    followees: true,
-    hash_id: 'd965f32a168564f9e58ad3a48a1585a4'
-}).then(function(result) {
-    console.log("success!!!");
-    console.log(result);
-})
+var getUserHashID = require('./getUserHashID');
+var Promise = require('bluebird');
+getUserHashID('https://www.zhihu.com/people/avit4799')
+    .then(function(hashID) {
+        var works = [Spider().request({
+            followees: true,
+            hash_id: hashID
+        }), Spider().request({
+            hash_id: hashID
+        })];
+        return Promise.all(works);
+    })
+    .then(function(result) {
+        console.log("fetch success!!!");
+        var followees = result[0].data;
+        var followers = result[1].data;
+        var friends = [];
+        followers.forEach(function(follower, index) {
+            followees.forEach(function(followee, index) {
+                if (follower.hash_id === followee.hash_id) {
+                    friends.push(follower);
+                }
+            });
+        });
+        console.log(friends);
+    });
